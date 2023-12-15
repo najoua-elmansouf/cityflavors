d804e24cfd41a75146a81a12660f842e7f0e6b61
gh-ita <hatimi.ghita@ensam-casa.ma>
2023-12-15 18:22:14 +0100

    responsive carte

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,29 @@ import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg nav">
+        <nav className="navbar navbar-expand-lg navbar-light ">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">
                 <img src={logo} alt="logo" style={{ width : '150px',height :'100px'}}/>
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse ville-container" id="navbarNavAltMarkup" >
-                    <div className="navbar-nav ville" >
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+                <div className="collapse navbar-collapse ville-container" id="navbarNav" >
+                    <ul className="navbar-nav ville" >
+                       <li class="nav-item">
                         <Link to="/Casablanca" className="nav-link active" style={{ color: '#990909', fontWeight: 'bold' }}>Casablanca</Link>
-                        <Link to="/Marrackech" className="nav-link active" style={{ color: '#990909', fontWeight: 'bold' }}>Marrackech</Link>
-                        <Link to="/Rabat" className="nav-link active" style={{ color: '#990909', fontWeight: 'bold' }}>Rabat</Link>
-                        <Link to="/Tanger" className="nav-link active" style={{ color: '#990909', fontWeight: 'bold' }}>Tanger</Link>
-                    </div>
+                        </li>
+                        <li class="nav-item">
+                        <Link to="/Marrackech" className="nav-link" style={{ color: '#990909', fontWeight: 'bold' }}>Marrackech</Link>
+                        </li>
+                        <li class="nav-item">
+                        <Link to="/Rabat" className="nav-link " style={{ color: '#990909', fontWeight: 'bold' }}>Rabat</Link>
+                        </li>
+                        <li class="nav-item">
+                        <Link to="/Tanger" className="nav-link" style={{ color: '#990909', fontWeight: 'bold' }}>Tanger</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
