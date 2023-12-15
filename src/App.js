3200c0a7608a900e9a27bcd39e6a4cd24bf83314
gh-ita <hatimi.ghita@ensam-casa.ma>
2023-12-15 17:06:51 +0100

    pagerepas done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/footer.jsx';
 import { Routes, Route } from 'react-router-dom';
 import Acceuil from './components/acceuil.jsx';
 import Pageville from './components/pageville.jsx';
+import Pagerepas from './components/pagerepas.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/Marrackech" element={<Pageville ville='marrackech'/>} />
         <Route path="/Rabat" element={<Pageville ville='rabat'/>} />
         <Route path="/Tanger" element={<Pageville ville='tanger'/>} />
+        <Route path="/recipe/:name" element={<Pagerepas/>} />
       </Routes>
     <Footer/>
     
---
+++ src/components/pagerepas.jsx
@@ -0,0 +1,44 @@
+import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+
+
+const Pagerepas = () => {
+    const { name } = useParams();
+    const { ville } = useParams();
+    const [repas, setRepas] = useState('');
+    const [image, setImage] = useState('');
+    const [ingrd, setIngredient] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`../../json/${ville}.json`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        const data = await response.json();
+        const desc = data[name].ingredient;
+        const img = data[name].image;
+        const repas = data[name].nom;
+        setRepas(repas);
+        setIngredient(desc);
+        setImage(img);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [ville]);
+
+    return (
+      <div className = 'repasContainer'>
+        <div className = 'repasSubContainer'>
+        <img src ={image}/>
+        <p><h1>{repas}</h1>{ingrd}</p>
+        </div>
+    </div>
+    );
+}
+
+export default Pagerepas
